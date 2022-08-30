cdfd3458099aa2aadbfea4ba96c12614592aaaf3
dhoniaridho <dhoniaridho@gmail.com>
2022-08-30 21:33:42 +0700

    feat(users): delete user

--- src/layouts/default.layout.vue
+++ src/layouts/default.layout.vue
@@ -2,6 +2,7 @@
   import DEFAULT_AVATAR from '~/assets/images/avatar.png'
   import DEFAULT_LOGO from '~/assets/images/logo.png'
   import POWERED_BY from '~/assets/images/logo-powered-by.svg'
+  import { Icon } from '@iconify/vue'
   import {
     mainMenuOptions,
     altMenuOptions,
@@ -36,36 +37,7 @@
             :wrap-item="false"
           >
             <!-- logo -->
-            <!-- <template v-if="auth.company.logo">
-              <router-link to="/document">
-                <n-image
-                  preview-disabled
-                  object-fit="contain"
-                  height="50"
-                  :src="auth.company.logo"
-                  :img-props="{
-                    style: { width: '100%', paddingBlock: '1rem' },
-                  }"
-                  style="pointer-events: none; width: 100%"
-                  draggable="false"
-                />
-              </router-link>
-              <n-space justify="end">
-                <a
-                  href="https://momofin.com/momofin-go/"
-                  target="_blank"
-                  rel="noreferer"
-                >
-                  <n-image
-                    preview-disabled
-                    object-fit="contain"
-                    height="24"
-                    style="pointer-events: none"
-                    :src="POWERED_BY"
-                  />
-                </a>
-              </n-space>
-            </template> -->
+
             <router-link to="/document">
               <n-image
                 object-fit="contain"
@@ -137,17 +109,17 @@
                   quaternary
                   style="padding-left: 0; padding-right: 12px"
                 >
-                  <!-- <n-space align="center">
+                  <n-space align="center">
                     <n-avatar
-                      v-if="auth.user.avatar"
+                      v-if="DEFAULT_AVATAR"
                       round
-                      :src="auth.user.avatar"
+                      :src="DEFAULT_AVATAR"
                       :size="32"
                     />
                     <n-avatar v-else round :src="DEFAULT_AVATAR" :size="32" />
-                    {{ auth.user.fullname }}
-                    <n-icon><iArrowDown1Icon /> </n-icon>
-                  </n-space> -->
+                    eMET
+                    <n-icon><Icon icon="heroicons:chevron-down" /> </n-icon>
+                  </n-space>
                 </n-button>
               </template>
               <n-menu
