46e43dc681ee30a6d21497d0e8c9c781908a0dbb
Fatkhurrahman <fatkhurrahmandjogja@gmail.com>
2022-09-26 15:54:50 +0700

    add modify

--- deployment/docker/run.sh
+++ deployment/docker/run.sh
@@ -2,8 +2,6 @@ docker rm -f dev-emet-dashboard
 docker run -d \
     --name dev-emet-dashboard \
     -p 81:80 \
-    -e VITE_API_BASE_URL="https://api-dev.momofin.com/emet_dashboard" \
-    -e TOKEN_STORAGE_KEY="{{ token-storage-key }}" \
     --restart always \
     --pull=always \
     ghcr.io/momofin/dev-emet-dashboard:latest
