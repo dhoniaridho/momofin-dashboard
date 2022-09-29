2cfacdc5b422d276a4ae3585b5fcc03234eaad42
dhoniaridho <dhoniaridho@gmail.com>
2022-09-29 12:50:37 +0700

    fix(document): add field size to table

--- src/modules/http.ts
+++ src/modules/http.ts
@@ -4,7 +4,7 @@ import type { App } from 'vue'
 
 export const http = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
-  timeout: 10000,
+  timeout: 30000,
   headers: {
     'Content-Type': 'application/json',
   },
